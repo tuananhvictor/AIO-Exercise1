8a28e015900fde20b3aa5e264c2fa5dccfc0ea08
victor <tuananhvictor03625@gmail.com>
2024-06-23 15:18:15 +0700

    a

--- .vscode/BTVN-WEEK3-MODULE1/queue_1.py
+++ .vscode/BTVN-WEEK3-MODULE1/queue_1.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-class MyStack:
+class MyQueue:
   def intialization(self, capacity):
-    self.capacity = [' ' for i in range(capacity)]
+    self.capacity = [' ' for _ in range(capacity)]
   def is_empty(self):
     for i in self.capacity:
       if i != ' ':
@@ -16,17 +16,18 @@ class MyStack:
       return True
     else:
       return False
-  def push(self,number):
+  def enqueue(self, number):
     for i in range(len(self.capacity)):
       if self.capacity[i]== ' ':
         self.capacity[i] = number
         break
-  def pop(self):
-    for i in range(len(self.capacity)-1,-1,-1):
+  def dequeue(self):
+    for i in range(len(self.capacity)):
       if self.capacity[i] != ' ':
         self.capacity[i] = ' '
-        break
-  def top(self):
-    for i in range(len(self.capacity) - 1, -1, -1):
+        return self.capacity[i]
+  def font(self):
+    for i in range(len(self.capacity)):
       if self.capacity[i] != ' ':
         return self.capacity[i]
+    
---
+++ .vscode/BTVN-WEEK3-MODULE1/stack.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+class MyStack:
+  def intialization(self, capacity):
+    self.capacity = [' ' for _ in range(capacity)]
+  def is_empty(self):
+    for i in self.capacity:
+      if i != ' ':
+        return False
+    return True
+  def is_full(self):
+    count = 0
+    for i in self.capacity:
+      if i != ' ':
+        count += 1
+    if count == len(self.capacity):
+      return True
+    else:
+      return False
+  def push(self,number):
+    for i in range(len(self.capacity)):
+      if self.capacity[i]== ' ':
+        self.capacity[i] = number
+        break
+  def pop(self):
+    for i in range(len(self.capacity)-1,-1,-1):
+      if self.capacity[i] != ' ':
+        self.capacity[i] = ' '
+        break
+  def top(self):
+    for i in range(len(self.capacity) - 1, -1, -1):
+      if self.capacity[i] != ' ':
+        return self.capacity[i]
